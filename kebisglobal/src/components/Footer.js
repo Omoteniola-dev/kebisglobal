fa0f1bf3ed340401970555103bf9636b1e4a8c77
Omoteniola-dev <fatunmbiteniola001@gmail.com>
2021-02-19 14:03:43 +0100

    Added footer component

---
+++ kebisglobal/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Cta from "./Cta";
+
+const Footer = () => {
+    return(
+        <div className="relative h-full mt-12">
+                <Cta />
+            <p className="text-center text-sm lg:text-base">2021 Copyright &copy; Kebisglobal. Built by <a className="text-purple-600" target="_blank"
+             rel="noreferrer" href="https://twitter.com/Omoteniola_dev">Omoteniola-dev</a></p>
+        </div>
+    )
+}
+
+export default Footer;
