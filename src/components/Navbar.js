ffbf48b15ee7e43ab167d746d1a58261548ea0df
Omoteniola-dev <fatunmbiteniola001@gmail.com>
2021-02-26 12:18:30 +0100

    changed the styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
     
       return (
         <div>
-            <nav className="w-screen bg-blue-500 lg:px-10 sm:px-10 px-2 sm:flex justify-between border-gray-600 z-10">
+            <nav className="w-screen bg-gray-100 lg:px-10 sm:px-10 px-2 sm:flex justify-between border-gray-600 z-10">
                 <div className="flex px-4 justify-between sm:py-0 pb-1">
                     <img
                      src="https://www.ravilamkoti.codes/static/b7742c4d36a8aad0eae9e469b8af55d5/55abd/logo.png"
@@ -28,18 +28,18 @@ const Navbar = () => {
                 </div>
         
             <ul className="hidden sm:flex cursor-pointer" id="navContent">
-                <li className="py-5 px-5 hover:text-blue-300 transition duration-200 hover:bg-gray-800 sm:hover:bg-transparent text-white">
+                <li className="py-5 px-5 hover:text-blue-300 transition duration-200 hover:bg-gray-800 sm:hover:bg-transparent text-blue-700">
                 <Link to="/">
                     <p className="text-base antialiased">Home</p>
                 </Link>
                 
                 </li>
-                <li className="text-white py-5 px-5 sm:border-b-2 border-transparent hover:text-blue-300 sm:hover:text-blue-300 transition hover:bg-gray-800 duration-200 sm:hover:bg-transparent">
+                <li className="text-blue-700 py-5 px-5 sm:border-b-2 border-transparent hover:text-blue-300 sm:hover:text-blue-300 transition hover:bg-gray-800 duration-200 sm:hover:bg-transparent">
                 <Link to="/About">
                     <p className="text-base antialiased">About Us</p>
                 </Link>
                 </li>
-                <li className="text-white py-5 px-5 sm:border-b-2 border-transparent hover:text-blue-300 sm:hover:text-blue-300 transition hover:bg-gray-800 duration-200 sm:hover:bg-transparent">
+                <li className="text-blue-700 py-5 px-5 sm:border-b-2 border-transparent hover:text-blue-300 sm:hover:text-blue-300 transition hover:bg-gray-800 duration-200 sm:hover:bg-transparent">
                 <Link to="/" id="dropdown" >
                     <p className="text-base antialiased" >Services</p>
                     <div className="dropdown-content">
@@ -51,19 +51,19 @@ const Navbar = () => {
                 </Link>
                 
                 </li>
-                <li className="text-white py-5 px-5 sm:border-b-2 border-transparent hover:text-blue-300 sm:hover:text-blue-300 transition hover:bg-gray-800 duration-200 sm:hover:bg-transparent">
+                <li className="text-blue-700 py-5 px-5 sm:border-b-2 border-transparent hover:text-blue-300 sm:hover:text-blue-300 transition hover:bg-gray-800 duration-200 sm:hover:bg-transparent">
                 <Link to="/Testimonialpage">
                     <p className="text-base antialiased">Testimonials</p>
                 </Link>
                 
                 </li>
-                <li className="text-white py-5 px-5 sm:border-b-2 border-transparent hover:text-blue-300 sm:hover:text-blue-300 transition hover:bg-gray-800 duration-200 sm:hover:bg-transparent">
+                <li className="text-blue-700 py-5 px-5 sm:border-b-2 border-transparent hover:text-blue-300 sm:hover:text-blue-300 transition hover:bg-gray-800 duration-200 sm:hover:bg-transparent">
                 <Link to="/Faq">
                     <p className="text-base antialiased">FAQ</p>
                 </Link>
                 
                 </li>
-                <li className="text-white py-5 px-5 sm:border-b-2 border-transparent hover:text-blue-300 sm:hover:text-blue-300 transition hover:bg-gray-800 duration-200 sm:hover:bg-transparent">
+                <li className="text-blue-700 py-5 px-5 sm:border-b-2 border-transparent hover:text-blue-300 sm:hover:text-blue-300 transition hover:bg-gray-800 duration-200 sm:hover:bg-transparent">
                 <Link to="/Contactpage">
                     <p className="text-base antialiased">Contact Us</p>
                 </Link>
