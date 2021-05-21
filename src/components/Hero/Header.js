8726c707b41847944d138bcb76f08c987bad9877
Teniola <fatunmbiteniola001@gmail.com>
2021-05-21 23:35:21 +0100

    modified the links

--- src/components/Hero/Header.js
+++ src/components/Hero/Header.js
@@ -7,20 +7,20 @@ const arr = [
 ]
 const Header = () => {
     return (
-        <div className="h-screen pt-4 m-12 lg:m-14">
+        <div className="h-screen pt-4 m-10 lg:m-14">
             <div className="lg:font-bold text-left mt-8 w-2/3 leading-normal 
             lg:m-0 lg:font-extrabold lg:w-2/3 lg:leading-snug">
             <p className="sm:py-8 text-4xl font-bold text-black-900 text-center leading-snug" 
-            style={{"fontSize":"60px", "fontWeight":"bolder"}}> 
+            style={{"fontSize":"40px", "fontWeight":"bolder"}}> 
             Better <small style={{"color":"blue"}}>IT solutions</small> for your business</p>
-            <p className="text-sm text-gray-500 w-full lg:w-auto">We are a team of seasoned IT professionals creating cutting edge solutions.</p>
+            <p className="text-sm text-gray-500 lg:w-full lg:w-auto">We are a team of seasoned IT professionals creating cutting edge solutions.</p>
             <div className="flex flex-wrap lg:flex-wrap lg:w-full ">
                     {  
-                        arr.map((a) => {
+                        arr.map((a, i) => {
                             return(
-                                <div className="bg-blue-400 m-2 w-3/4 lg:m-5 p-4 lg:p-4 lg:w-1/4 lg:mt-8 text-center font-bold text-xl rounded-xl hover:bg-blue-500">
+                                <div className="bg-blue-400 m-2 w-3/4 lg:m-5 p-3 lg:p-3 lg:w-1/4 lg:mt-8 text-center rounded-lg hover:bg-blue-500">
                                     <Link to={a.href}>
-                                        <p className="antialiased font-bold">{a.text}</p>
+                                        <p className="antialiased">{a.text}</p>
                                     </Link>
                                 </div>
                                 
