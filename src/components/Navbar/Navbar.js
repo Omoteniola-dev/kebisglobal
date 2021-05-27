890a442def740b8128ea08c4a850a2b7791c321c
Teniola <fatunmbiteniola001@gmail.com>
2021-05-27 22:25:01 +0100

    code refactored

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
+const navLinks = [
+    {"text": "Home", "link": "/"},
+    {"text": "About", "link": "/about"},
+    {"text": "Testimonials", "link": "/testimonials"},
+    {"text": "FAQ", "link": "/faq"},
+    {"text": "Contact", "link": "/contact"}
+];
+
+
 const Navbar = () => {
     const handleClick = () => {
         const element = document.querySelector("#navContent");
         element.classList.toggle("block");
         element.classList.toggle("hidden");
       };
-    
+
+
       return (
         <div>
             <nav className="w-screen bg-white lg:px-10 sm:px-10 px-2 sm:flex justify-between border-gray-600 z-10">
@@ -26,46 +37,15 @@ const Navbar = () => {
                 </div>
         
             <ul className="hidden sm:flex cursor-pointer" id="navContent">
-                <li className="py-5 px-5 hover:text-blue-300 transition duration-200 hover:bg-gray-800 sm:hover:bg-transparent text-blue-700">
-                <Link to="/">
-                    <p className="antialiased">Home</p>
-                </Link>
-                
-                </li>
-                <li className="text-blue-700 py-5 px-5 sm:border-b-2 border-transparent hover:text-blue-300 sm:hover:text-blue-300 transition hover:bg-gray-800 duration-200 sm:hover:bg-transparent">
-                <Link to="/AboutPage">
-                    <p className="antialiased">About Us</p>
-                </Link>
-                </li>
-                <li className="text-blue-700 py-5 px-5 sm:border-b-2 border-transparent hover:text-blue-300 sm:hover:text-blue-300 transition hover:bg-gray-800 duration-200 sm:hover:bg-transparent">
-                <Link to="" id="dropdown" >
-                    <p className="antialiased" >Services</p>
-                    <div className="dropdown-content">
-                        <p>Web design and development</p>
-                        <p>Business process automation</p>
-                        <p>Hardware sales</p>
-                        <p>ICT training</p>
-                    </div>
-                </Link>
-                
-                </li>
-                <li className="text-blue-700 py-5 px-5 sm:border-b-2 border-transparent hover:text-blue-300 sm:hover:text-blue-300 transition hover:bg-gray-800 duration-200 sm:hover:bg-transparent">
-                <Link to="/Testimonialpage">
-                    <p className="antialiased">Testimonials</p>
-                </Link>
-                
-                </li>
-                <li className="text-blue-700 py-5 px-5 sm:border-b-2 border-transparent hover:text-blue-300 sm:hover:text-blue-300 transition hover:bg-gray-800 duration-200 sm:hover:bg-transparent">
-                <Link to="/Faq">
-                    <p className="antialiased">FAQ</p>
-                </Link>
-                
-                </li>
-                <li className="text-gray-400 py-5 px-5 sm:border-b-2 border-transparent hover:text-blue-300 sm:hover:text-blue-300 transition hover:bg-gray-800 duration-200 sm:hover:bg-transparent">
-                <Link to="/Contactpage">
-                    <p className="antialiased">Contact Us</p>
-                </Link>
-                </li>
+               {
+                   navLinks.map((link, i) => {
+                       return <li key={i} className="py-5 px-5 hover:text-blue-300 transition duration-200 hover:bg-gray-800 sm:hover:bg-transparent text-blue-700">
+                                    <Link to={link.link}>
+                                        <p className="antialiased">{link.text}</p>
+                                    </Link>
+                                </li>
+                   })
+               } 
                 <li className="text-blue-700 py-4 px-5 sm:border-b-2 border-transparent hover:text-blue-600 sm:hover:text-blue-600 transition hover:bg-gray-900 bg-blue-300 rounded-xl m-1 duration-200">
                     <Link to="/">
                         <p className="antialiased">Request a quote</p>
