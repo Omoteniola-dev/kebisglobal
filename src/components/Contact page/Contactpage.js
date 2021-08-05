73674ee227224232210f9de95c4d574bbe0e435a
Teniola <fatunmbiteniola001@gmail.com>
2021-08-05 16:10:37 +0100

    redesigned contact page

--- src/components/Contact page/Contactpage.js
+++ src/components/Contact page/Contactpage.js
@@ -1,17 +1,18 @@
 import React from "react";
 import Form from "./Form";
+
+
+
+
 class Contactpage extends React.Component{
-    
-    
-    
     render(){
             return (
-            <div className="h-screen m-10 pt-4 lg:pt-4 lg:m-10 ">
+            <div className="m-10 pt-4 lg:pt-4 lg:m-10 ">
                 <h1 className="text-2xl text-center lg:text-4xl font-xl">Reach Out!</h1>
                 <p className="text-sm text-gray-500 text-center">We love questions and feedbacks - We are always happy to help!
                     Here are some ways to contact us.</p>
-                    <div className="flex justify-center">
-                    <Form />
+                    <div className="lg:ml-96">
+                        <Form />
                     </div>
             </div>
         )
